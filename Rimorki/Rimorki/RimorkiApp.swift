1b56cf83137d6cae2a8d7f4bd2d957ebd4d5efe3
B e n g i <bambang.trd17@gmail.com>
2024-01-16 16:31:20 +0700

    chore: initialize required files from XCode

---
+++ Rimorki/Rimorki/RimorkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RimorkiApp.swift
+//  Rimorki
+//
+//  Created by Bambang Tri Rahmat Doni on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RimorkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
