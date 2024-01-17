ba8f1f0fba505fcfb211b218efa17cdf605bd0e9
B e n g i <bambang.trd17@gmail.com>
2024-01-17 13:27:44 +0700

    chore: add shared enum for UserDefaults keys

---
+++ Rimorki/Rimorki/Shared/AppUserDefaults.swift
@@ -0,0 +1,12 @@
+//
+//  AppUserDefaults.swift
+//  Rimorki
+//
+//  Created by Bambang Tri Rahmat Doni on 17/01/24.
+//
+
+import Foundation
+
+enum AppUserDefaults: String, Hashable, CaseIterable {
+    case hasOnboardingCompleted
+}
