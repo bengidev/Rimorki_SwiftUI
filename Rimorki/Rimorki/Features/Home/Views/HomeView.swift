0d4dc7d2d7237dd0689f04ce963a6914cdf0471a
B e n g i <bambang.trd17@gmail.com>
2024-01-17 16:41:39 +0700

    refactor: separate specified views into components

--- Rimorki/Rimorki/Features/Home/Views/HomeView.swift
+++ Rimorki/Rimorki/Features/Home/Views/HomeView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var tabItems: [String] = []
-    @State private var selectedTab = 0
+    @State private var selectedTabCategory: AppTabCategory = .home
     
     init() {
-        // Hide the default UITabBar
+        // Hide the default UITabBar appearance
+        //
         UITabBar.appearance().isHidden = true
     }
     
@@ -27,65 +28,40 @@ struct HomeView: View {
                     .background(Color.gray)
             }
             
-            Spacer(minLength: 10.0)
-            
             HStack(spacing: 0) {
-                Button {
-                    withAnimation(.easeInOut) {
-                        selectedTab = 0
-                    }
-                } label: {
-                    HStack(spacing: 8.0) {
-                        Image(systemName: "house.fill")
-                            .foregroundStyle(selectedTab == 0 ? Color.white : Color.black.opacity(0.35))
-                            .padding(10.0)
-                            .background(selectedTab == 0 ? Color.black : Color.clear)
-                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                        
-                        Text(selectedTab == 0 ? "Home": "")
-                            .foregroundStyle(Color.black)
-                    }
+                AppTabItemView(
+                    tabItemName: "Home",
+                    tabItemImage: "house.fill",
+                    selectedTabItem: selectedTabCategory,
+                    tabItemTag: .home
+                ) { tabCategory in
+                    withAnimation { selectedTabCategory = tabCategory }
                 }
                 
                 Spacer(minLength: 0)
                 
-                Button {
-                    withAnimation(.easeInOut) {
-                        selectedTab = 1
-                    }
-                } label: {
-                    HStack(spacing: 8.0) {
-                        Image(systemName: "house.fill")
-                            .foregroundStyle(selectedTab == 1 ? Color.white : Color.black.opacity(0.35))
-                            .padding(10.0)
-                            .background(selectedTab == 1 ? Color.black : Color.clear)
-                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                        
-                        Text(selectedTab == 1 ? "Home": "")
-                            .foregroundStyle(Color.black)
-                    }
+                AppTabItemView(
+                    tabItemName: "Favorite",
+                    tabItemImage: "star.fill",
+                    selectedTabItem: selectedTabCategory,
+                    tabItemTag: .favorite
+                ) { tabCategory in
+                    withAnimation { selectedTabCategory = tabCategory }
                 }
                 
                 Spacer(minLength: 0)
                 
-                Button {
-                    withAnimation(.easeInOut) {
-                        selectedTab = 2
-                    }
-                } label: {
-                    HStack(spacing: 8.0) {
-                        Image(systemName: "house.fill")
-                            .foregroundStyle(selectedTab == 2 ? Color.white : Color.black.opacity(0.35))
-                            .padding(10.0)
-                            .background(selectedTab == 2 ? Color.black : Color.clear)
-                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-                        
-                        Text(selectedTab == 2 ? "Home": "")
-                            .foregroundStyle(Color.black)
-                    }
+                AppTabItemView(
+                    tabItemName: "Account",
+                    tabItemImage: "person.fill",
+                    selectedTabItem: selectedTabCategory,
+                    tabItemTag: .account
+                ) { tabCategory in
+                    withAnimation { selectedTabCategory = tabCategory }
                 }
             }
-            .padding(.horizontal)
+            .padding(.all)
+            .background(Color.appSecondary)
         }
     }
 }
