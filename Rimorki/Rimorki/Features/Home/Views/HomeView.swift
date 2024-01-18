5e9a5a9daa50e46585f34f8766eb1cd7ffbace51
B e n g i <bambang.trd17@gmail.com>
2024-01-18 15:17:32 +0700

    refactor: load DashboardView inside HomeView

--- Rimorki/Rimorki/Features/Home/Views/HomeView.swift
+++ Rimorki/Rimorki/Features/Home/Views/HomeView.swift
@@ -20,10 +20,11 @@ struct HomeView: View {
     var body: some View {
         VStack(spacing: 0) {
             TabView(selection: .constant(1)) {
-                Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+                DashboardView().tag(1)
+                Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(2)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color.gray)
-                Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+                Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(3)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .background(Color.gray)
             }
