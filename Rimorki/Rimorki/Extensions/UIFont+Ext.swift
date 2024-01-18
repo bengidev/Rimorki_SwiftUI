3207b30d4f95c8939d4646329aa3c7c94c895916
B e n g i <bambang.trd17@gmail.com>
2024-01-18 20:33:43 +0700

    chore: add extensions for UIFont UIKit

---
+++ Rimorki/Rimorki/Extensions/UIFont+Ext.swift
@@ -0,0 +1,51 @@
+//
+//  UIFont+Ext.swift
+//  Rimorki
+//
+//  Created by Bambang Tri Rahmat Doni on 18/01/24.
+//
+
+import UIKit
+
+extension UIFont {
+    func withTraits(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        guard let descriptor = fontDescriptor.withSymbolicTraits(traits) else {
+            return self
+        }
+        
+        return UIFont(descriptor: descriptor, size: pointSize)
+    }
+    
+    func bold() -> UIFont {
+        return withTraits(traits: .traitBold)
+    }
+    
+    func italic() -> UIFont {
+        return withTraits(traits: .traitItalic)
+    }
+    
+    func rounded() -> UIFont {
+        guard let descriptor = fontDescriptor.withDesign(.rounded) else {
+            return self
+        }
+        
+        return UIFont(descriptor: descriptor, size: pointSize)
+    }
+    
+    func monospaced() -> UIFont {
+        guard let descriptor = fontDescriptor.withDesign(.monospaced) else {
+            return self
+        }
+        
+        return UIFont(descriptor: descriptor, size: pointSize)
+    }
+    
+    func serif() -> UIFont {
+        guard let descriptor = fontDescriptor.withDesign(.serif) else {
+            return self
+        }
+        
+        return UIFont(descriptor: descriptor, size: pointSize)
+    }
+}
+
