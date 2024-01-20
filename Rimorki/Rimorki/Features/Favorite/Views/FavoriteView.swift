7ad90b5b4d78f66b96b637bbc10c9247375a925a
B e n g i <bambang.trd17@gmail.com>
2024-01-20 13:46:30 +0700

    fix: change view values from main thread

--- Rimorki/Rimorki/Features/Favorite/Views/FavoriteView.swift
+++ Rimorki/Rimorki/Features/Favorite/Views/FavoriteView.swift
@@ -5,61 +5,85 @@
 //  Created by Bambang Tri Rahmat Doni on 18/01/24.
 //
 
+import Kingfisher
 import SwiftUI
 
 struct FavoriteView: View {
-    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    private let viewModel: FavoriteViewModel = .init()
+    @State private var rickMortyCollections: [RickMortyCDModel] = []
+    @State private var convertedRickMortyModel: RickMortyAPIModel.Result = .empty
+    @State private var isFavoriteModel: Bool = false
+    @State private var isSheetPresented: Bool = false
     @State private var searchText = ""
+    
+    private var searchResults: [RickMortyCDModel] {
+        if searchText.isEmpty {
+            return rickMortyCollections
+        } else {
+            return rickMortyCollections.filter { $0.wrappedName.contains(searchText) }
+        }
+    }
     
     var body: some View {
         GeometryReader { geo in
             NavigationView {
                 List {
-                    ForEach(searchResults, id: \.self) { name in
-                        HStack {
-                            Image(.rick)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: geo.size.width * 0.07)
-                                .padding(.vertical, 5.0)
-                                .padding(.horizontal)
-                                .shadow(color: .black, radius: 10.0)
-                                .drawingGroup()
-                            
-                            VStack (alignment: .leading) {
-                                Text("Name: Rick")
-                                    .font(.system(.headline, design: .rounded).bold())
+                    ForEach(searchResults) { item in
+                        Button {
+                            DispatchQueue.main.async {
+                                convertedRickMortyModel = viewModel.convertRickMortyCDtoAPI(item)
+                                isFavoriteModel = item.isFavorite
                                 
-                                Text("Status: Alive")
-                                    .font(.system(.subheadline, design: .rounded))
+                                isSheetPresented.toggle()
                             }
-                            
-                            Spacer()
+                        } label: {
+                            HStack {
+                                let url = URL(string: item.wrappedImage)!
+                                let processor = DownsamplingImageProcessor(size: geo.size)
+                                |> RoundCornerImageProcessor(cornerRadius: 10.0)
+                                
+                                KFImage.url(url)
+                                    .placeholder({ Image.init(systemName: "hourglass") })
+                                    .setProcessor(processor)
+                                    .loadDiskFileSynchronously()
+                                    .cacheMemoryOnly()
+                                    .fade(duration: 0.25)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                VStack (alignment: .leading) {
+                                    Text(item.wrappedName)
+                                        .font(.system(.footnote, design: .rounded).bold())
+                                    
+                                    Text("Status: \(item.wrappedStatus)")
+                                        .font(.system(.caption, design: .rounded))
+                                }
+                                
+                                Spacer()
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: geo.size.height * 0.08)
+                            .background(Color.appPrimary)
+                            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                            .shadow(radius: 3.0)
                         }
-                        .frame(width: .infinity, height: geo.size.height * 0.08)
-                        .background(Color.appPrimary)
-                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        .buttonStyle(.plain)
                         .listRowSeparator(.hidden)
                         .listRowInsets(.init(top: 5.0, leading: 5.0, bottom: 5.0, trailing: 5.0))
                         .listRowBackground(Color.appSecondary)
                     }
                 }
+                .searchable(text: $searchText)
                 .listBackgroundColor(.appSecondary)
                 .navigationTitle("Your Favorites")
             }
-            .searchable(text: $searchText) {
-                ForEach(searchResults, id: \.self) { result in
-                    Text("Are you looking for \(result)?").searchCompletion(result)
-                }
+        }
+        .onAppear {
+            DispatchQueue.main.async {
+                rickMortyCollections = viewModel.fetchRickMortyCharacters()
             }
         }
-    }
-    
-    var searchResults: [String] {
-        if searchText.isEmpty {
-            return names
-        } else {
-            return names.filter { $0.contains(searchText) }
+        .fullScreenCover(isPresented: $isSheetPresented) {
+            DetailView(rickMortyModel: convertedRickMortyModel, isFavoriteModel: isFavoriteModel)
         }
     }
 }
